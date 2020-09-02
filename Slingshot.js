3a692508de9a7c8bb541ac6a2fde1e7c2b2f8c38
Tarun-Ultimate <68709064+Tarun-Ultimate@users.noreply.github.com>
2020-09-01 19:41:46 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,49 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            if(pointA.x < 220){
+                push(); 
+                strokeWeight(7);
+                stroke(48,22,8)
+                line(pointA.x,pointA.y,pointB.x,pointB.y);
+                line(pointA.x,pointA.y,pointB.x+40,pointB.y);
+                image(this.sling3,pointA.x-10,pointA.y,15,30);
+                pop(); 
+            }
+            else{
+                push(); 
+                strokeWeight(4);
+                stroke(48,22,8)
+                line(pointA.x,pointA.y,pointB.x,pointB.y);
+                line(pointA.x,pointA.y,pointB.x+40,pointB.y);
+                image(this.sling3,pointA.x-10,pointA.y,15,30);
+                pop();  
+            }
+        }
+    }
+    
+}
